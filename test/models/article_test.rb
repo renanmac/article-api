1c527ad6ccea5e1f7e895a80054ad27c84d15352
renanmacs <renanmacs@gmail.com>
2018-08-03 13:41:19 -0300

    Added presence validation and testing for the model Article

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @article = Article.new
+  end
+
+  test "should not article without Title" do
+    assert_not @article.save, "Saved article without title"
+  end
+
+  test "should not article without Content" do
+    assert_not @article.save, "Saved article without content"
+  end
+
+  test "should create article" do
+    article = articles(:one)
+    assert Article.new(title: article.title, content: article.content)
+  end
+
 end
