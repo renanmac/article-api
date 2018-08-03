820a68ff9868f192bfbe9e12bf13aadcc914a6e3
renanmacs <renanmacs@gmail.com>
2018-08-03 13:49:29 -0300

    Added Article controller and Routes

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,43 @@
+class Api::V1::ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :update, :delete]
+
+  def index
+    @articles = Article.all
+    render json: @articles
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      render json: @article, status: :created
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: @article
+  end
+
+  def update
+    if @article.update(article_params)
+      render json: @article
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def delete
+    @article.destroy
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])  
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
